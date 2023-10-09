0bf5a99bf14ed74bf267edcc399ac0a2003cb2d1
Fady Mondy <EngFadyMondy@gmail.com>
2023-10-09 22:22:20 +0300

    first commit 🔥

---
+++ README.md
@@ -0,0 +1,64 @@
+# Tomato flutter
+
+your package description will go here
+
+## Installation
+
+```bash
+composer require tomatophp/tomato-flutter
+```
+after install your package please run this command
+
+```bash
+php artisan tomato-flutter:install
+```
+
+## Publish Assets
+
+you can publish config file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-flutter-config"
+```
+
+you can publish views file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-flutter-views"
+```
+
+you can publish languages file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-flutter-lang"
+```
+
+you can publish migrations file by use this command
+
+```bash
+php artisan vendor:publish --tag="tomato-flutter-migrations"
+```
+
+## Support
+
+you can join our discord server to get support [TomatoPHP](https://discord.gg/Xqmt35Uh)
+
+## Docs
+
+you can check docs of this package on [Docs](https://docs.tomatophp.com/plugins/laravel-package-generator)
+
+## Changelog
+
+Please see [CHANGELOG](CHANGELOG.md) for more information on what has changed recently.
+
+## Security
+
+Please see [SECURITY](SECURITY.md) for more information about security.
+
+## Credits
+
+- [Tomatophp](mailto:info@3x1.io)
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/TomatoFlutterServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tomatophp\TomatoFlutter;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class TomatoFlutterServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //Register generate command
+        $this->commands([
+           \Tomatophp\TomatoFlutter\Console\TomatoFlutterInstall::class,
+        ]);
+ 
+        //Register Config file
+        $this->mergeConfigFrom(__DIR__.'/../config/tomato-flutter.php', 'tomato-flutter');
+ 
+        //Publish Config
+        $this->publishes([
+           __DIR__.'/../config/tomato-flutter.php' => config_path('tomato-flutter.php'),
+        ], 'tomato-flutter-config');
+ 
+        //Register Migrations
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+ 
+        //Publish Migrations
+        $this->publishes([
+           __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'tomato-flutter-migrations');
+        //Register views
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'tomato-flutter');
+ 
+        //Publish Views
+        $this->publishes([
+           __DIR__.'/../resources/views' => resource_path('views/vendor/tomato-flutter'),
+        ], 'tomato-flutter-views');
+ 
+        //Register Langs
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'tomato-flutter');
+ 
+        //Publish Lang
+        $this->publishes([
+           __DIR__.'/../resources/lang' => app_path('lang/vendor/tomato-flutter'),
+        ], 'tomato-flutter-lang');
+ 
+        //Register Routes
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+ 
+    }
+
+    public function boot(): void
+    {
+        //you boot methods here
+    }
+}
